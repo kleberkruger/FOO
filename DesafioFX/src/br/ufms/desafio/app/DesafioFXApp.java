6daf60d6d08d3986fbfac2b41ca0212e8342b494
Kleber Kruger <kleberkruger@gmail.com>
2016-06-04 04:54:22 -0400

    Commit inicial do projeto.
    Projeto JavaFX com as estruturas dos pacotes e a classe DesafioFXApp.

---
+++ DesafioFX/src/br/ufms/desafio/app/DesafioFXApp.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2016 kleberkruger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.ufms.desafio.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author kleberkruger
+ */
+public class DesafioFXApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+        
+        // Carrega arquivo fxml do pacote view.fxml
+        Parent root = (Parent) loader.load(getClass().getClassLoader().getResourceAsStream(
+                "br/ufms/desafio/view/fxml/Login.fxml"));
+
+        Scene scene = new Scene(root);
+        
+        // Adiciona o arquivo CSS para a personalização desta cena
+        scene.getStylesheets().add(getClass().getClassLoader().getResource(
+                "br/ufms/desafio/view/css/desafio.css").toExternalForm());
+
+        // Altera o título da janela para "DesafioFX", define a cena e a exibe na tela
+        stage.setTitle("DesafioFX");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ DesafioFX/src/br/ufms/desafio/model/Usuario.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2016 kleberkruger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.ufms.desafio.model;
+
+/**
+ *
+ * @author kleberkruger
+ */
+public class Usuario {
+    
+    protected String nome;
+    protected String usuario;
+    protected String senha;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
